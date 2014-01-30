f9b610b343fa0c0a099dcfd3d1bf890e9296d9bf
Marko Marković <marko.markovic@bbv.ch>
2014-01-30 17:12:27 +0100

    Added Ninject.Web package.

---
+++ bbv.MvpSimple/App_Start/NinjectWeb.cs
@@ -0,0 +1,34 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NinjectWeb.cs" company="bbv Software Services AG">
+//   Copyright (c) 2014
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(bbv.MvpSimple.App_Start.NinjectWeb), "Start")]
+
+namespace bbv.MvpSimple.App_Start
+{
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject.Web;
+
+    public static class NinjectWeb 
+    {
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+        }
+    }
+}
---
+++ bbv.MvpSimple/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,59 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NinjectWebCommon.cs" company="bbv Software Services AG">
+//   Copyright (c) 2014
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(bbv.MvpSimple.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(bbv.MvpSimple.App_Start.NinjectWebCommon), "Stop")]
+
+namespace bbv.MvpSimple.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+            RegisterServices(kernel);
+            return kernel;
+        }
+
+        private static void RegisterServices(IKernel kernel)
+        {
+        }
+    }
+}
